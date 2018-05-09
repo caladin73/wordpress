5d6b8a1c4927a685ab3914d9d97525647e226797
Peter <myrvig@gmail.com>
2018-05-09 11:57:00 +0200

    op

---
+++ plugins/alecadd-plugin/alecadd-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package AllecaddPlugin
+ */
+/*
+Plugin Name: Alecadd Plugin
+Plugin URL: http://alecadd.com/plugin
+Description: This is my first attempt on writing custom PLugin for this amazing plugin
+Version 1.0.0
+Authot: Peter
+Author URL: htpp://allecadd.com
+License: GPLv3 or later
+Text Domain: allecadd-plugin
+*/
+
---
+++ wp-content/ari-adminer-config.php
@@ -0,0 +1,3 @@
+<?php
+defined( 'ABSPATH' ) or die( 'Access forbidden!' );
+define( 'ARIADMINER_CRYPT_KEY', 'bbfd173ad0b3de4a57e2f0fc88e74f4b' );
